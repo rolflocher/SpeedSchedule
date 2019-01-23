7303c63ab72ed24c06a78a5a94132045dc2c6443
Rolf Locher <rlocher2@mac.com>
2019-01-22 22:37:23 -0500

    Initial Commit

---
+++ SpeedSchedule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpeedSchedule
+//
+//  Created by Rolf Locher on 1/22/19.
+//  Copyright © 2019 Rolf Locher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
